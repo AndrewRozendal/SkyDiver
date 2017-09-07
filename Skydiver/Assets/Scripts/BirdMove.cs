b6608449ba679b00efc0ff26a4d739e6eb2a5f57
unknown <C0431029@TB147-03.intra.camosun.bc.ca>
2017-09-06 17:09:17 -0700

    Replaced killwall for enemies with code logic in BirdMove.cs, replaced player boundary with code logic in FPSInput.cs

--- Skydiver/Assets/Scripts/BirdMove.cs
+++ Skydiver/Assets/Scripts/BirdMove.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public enum BirdState {Dead, Alive};
 
 public class BirdMove : MonoBehaviour {
-
+    public bool debugMode = true;
+    public float maxDistance = 100f;
 	public float birdSpeed;
 	public float pushForce = 5.0f;
 	private BirdState state;
@@ -18,33 +19,60 @@ public class BirdMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (state == BirdState.Alive){
-			transform.Translate (Vector3.back * birdSpeed * Time.deltaTime);
+        if (state == BirdState.Alive) {
+            //are we in bounds?
+            bool inbounds = true;
+            Transform location = this.GetComponent<Transform>();
+            if(location.position.x > maxDistance || location.position.x < maxDistance * -1)
+            {
+                if (debugMode) {
+                    Debug.Log("Bird was too far on x");
+                    Debug.Log("Position is: " + location.position);
+                }
+                inbounds = false;
+            } else if (location.position.y > maxDistance || location.position.y < maxDistance * -1)
+            {
+                if (debugMode) {
+                    Debug.Log("Bird was too far on y");
+                    Debug.Log("Position is: " + location.position);
+                }
+                inbounds = false;
+            }
+            else if (location.position.z > maxDistance || location.position.z < maxDistance * -1)
+            {
+                if (debugMode) {
+                    Debug.Log("Bird was too far on z");
+                    Debug.Log("Position is: " + location.position);
+                    Debug.Log("location.position.z > maxDistance evaluates to " + (location.position.z > maxDistance));
+                    Debug.Log("location.position.z < maxDistance * -1 evaluates to " + (location.position.z < maxDistance * -1));
+                    Debug.Log("maxDistance is " + maxDistance);
+                }
+                inbounds = false;
+            }
 
+            if (inbounds)
+            {
+                transform.Translate(Vector3.back * birdSpeed * Time.deltaTime);
+            } else
+            {
+                state = BirdState.Dead;
+            }	
 		}
+
 		if (state == BirdState.Dead) {
 			Die ();
 		}
 	}
 
+    //Changes the state of the bird
 	public void ChangeState(BirdState state){
 		this.state = state;
 		Debug.Log ("State= " + state);
 	}
-		
 
-
+    //Destroys the GameObject
 	public void Die (){
 		Destroy (this.gameObject);
 		Debug.Log ("Dead");
-
-	}
-	void OnControllerColliderHit(ControllerColliderHit hit){
-		Rigidbody body = hit.collider.attachedRigidbody;
-
-		if (body != null && !body.isKinematic) {
-			body.velocity = hit.moveDirection * pushForce;
-		}
-
 	}
 }
